43fc430d07dc276a3162626c4c7a232cfe4347b9
Lalit Rajak <lalitrajak312@gmail.com>
2024-07-15 01:07:12 +0530

    added the SecurityConfig

--- src/main/java/com/next/auth/config/OpenAPIConfig.java
+++ src/main/java/com/next/auth/config/OpenAPIConfig.java
@@ -2,6 +2,8 @@ package com.next.auth.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +16,9 @@ public class OpenAPIConfig {
                 .info(new Info()
                         .title("API Documentation")
                         .version("1.0")
-                        .description("API documentation for the application"));
+                        .description("API documentation for the application"))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(new io.swagger.v3.oas.models.Components().addSecuritySchemes("basicAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")));
     }
 }
--- src/main/java/com/next/auth/config/WebConfig.java
+++ src/main/java/com/next/auth/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
